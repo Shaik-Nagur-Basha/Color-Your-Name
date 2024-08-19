b36fa52251fad5b5742ea0df0de3b3ef9bb061d1
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-08-19 13:38:58 +0530

    custom elements added!

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-const name = prompt("Enter your Name: ");
-const nameSize = prompt("Enter the px SIZE in number: ");
-const colorInterval = prompt("Enter color interval in Seconds : ");
-const noOfElements = prompt("How many times your Name: ");
-//In further custom Element
-
-
-for (let i = 0; i < noOfElements; i++) {
-  let spanElement = document.createElement("span");
-  document.querySelector("div").appendChild(spanElement);
+function values(){
+  const name = document.querySelector('#name').value
+  const nameSize = document.querySelector('#nameSize').value
+  const colorInterval = document.querySelector('#colorInterval').value
+  const noOfElements = document.querySelector('#noOfElements').value
+  const htmlElement = document.querySelector('#htmlElement').value
+  if(name && nameSize && colorInterval && noOfElements && htmlElement) {
+    document.querySelector('#form').style.display = 'none'
+    return main(name, nameSize, colorInterval, noOfElements, htmlElement)
+  } else {
+    try {
+      document.querySelector('#form').removeAttributeNode('style')
+    } catch {
+      alert('Fill all Fields!')
+    }
+  }
 }
 
-const allElements = document.querySelectorAll("span");
+const value1 = document.querySelector("#value1");
+const input1 = document.querySelector("#nameSize");
+const value2 = document.querySelector("#value2");
+const input2 = document.querySelector("#colorInterval");
+value1.textContent = input1.value;
+input1.addEventListener("input", (event) => {
+  value1.textContent = event.target.value;
+});
+value2.textContent = input2.value;
+input2.addEventListener("input", (event) => {
+  value2.textContent = event.target.value;
+});
 
-for (let i = 0; i < allElements.length; i++) {
-  setInterval(() => {
-    function c() {
-      return Math.floor(Math.random() * 255);
-    }
-    allElements[i].innerHTML = `${name}`;
-    allElements[i].style.fontSize = `${nameSize + "px"}`;
-    allElements[i].style.color = `rgb(${c()}, ${c()}, ${c()})`;
-  }, `${colorInterval * 1000}`);
+function main(name, nameSize, colorInterval, noOfElements, htmlElement) {
+  for (let i = 0; i < noOfElements; i++) {
+    document.querySelector("#colorElements").appendChild(document.createElement(`${htmlElement}`));
+  }
+
+  const allElements = document.querySelectorAll(`${htmlElement}`);
+  
+  for (let i = 0; i < allElements.length; i++) {
+    setInterval(() => {
+      function c() {
+        return Math.floor(Math.random() * 255);
+      }
+      allElements[i].innerHTML = `${name}`;
+      allElements[i].style.fontSize = `${nameSize + "px"}`;
+      allElements[i].style.color = `rgb(${c()}, ${c()}, ${c()})`;
+    }, colorInterval * 1000);
+  }
 }
